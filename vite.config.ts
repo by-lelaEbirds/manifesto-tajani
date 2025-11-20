3221f9f5bb0cd9a8a89d32d985ee14dda223fad0
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-11-20 07:18:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/manifesto-tajani/', 
-  plugins: [react(), tailwindcss()],
-resolve: {
-  alias: {
-    "@": path.resolve(import.meta.dirname, "client", "src"),
+  base: '/manifesto-tajani/',
+  plugins: [react()], // Removi o tailwindcss() daqui, pois agora ele roda via PostCSS
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "client", "src"),
+    },
   },
-},
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   build: {
