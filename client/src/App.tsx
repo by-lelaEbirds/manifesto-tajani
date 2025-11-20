fcd67edd246e821e909e5f28a93b833d9b89b06f
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-11-20 11:04:27 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,15 @@ import { Route, Switch, Router } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import Dossier from "./pages/Dossier"; // <--- Importe o novo arquivo
+import Dossier from "./pages/Dossier";
+import Evidence from "./pages/Evidence"; // Importe a nova página
 
 function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/dossier" component={Dossier} /> {/* <--- Adicione esta rota */}
+      <Route path="/dossier" component={Dossier} />
+      <Route path="/evidence" component={Evidence} /> {/* Nova Rota */}
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
