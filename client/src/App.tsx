23cfee14bdd1e06b1a0d6f25f545630b37c45a31
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-11-21 20:29:30 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,17 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch, Router } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext"; // Importar
 import Home from "./pages/Home";
 import Dossier from "./pages/Dossier";
-import Evidence from "./pages/Evidence"; // Importe a nova página
+import Evidence from "./pages/Evidence";
 
 function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/dossier" component={Dossier} />
-      <Route path="/evidence" component={Evidence} /> {/* Nova Rota */}
+      <Route path="/evidence" component={Evidence} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
@@ -25,14 +26,16 @@ function App() {
 
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
-          <Toaster />
-          <Router base={basePath}>
-            <AppRoutes />
-          </Router>
-        </TooltipProvider>
-      </ThemeProvider>
+      <LanguageProvider> {/* Adicionar Provider de Linguagem */}
+        <ThemeProvider defaultTheme="dark">
+          <TooltipProvider>
+            <Toaster />
+            <Router base={basePath}>
+              <AppRoutes />
+            </Router>
+          </TooltipProvider>
+        </ThemeProvider>
+      </LanguageProvider>
     </ErrorBoundary>
   );
 }
